0d7c00f18b314675df1f32afa03cda62284bf225
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-13 22:15:39 +0545

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -13,12 +13,11 @@ rl.on("line", (command) => {
   if (command === "exit") {
     rl.close();
     return;
-  }
-  else if (command.startsWith("echo ")) {
+  } else if (command.startsWith("echo ")) {
     console.log(command.slice(5));
+  } else {
+    console.log(`${command}: command not found`);
   }
-
-  console.log(`${command}: command not found`);
   rl.prompt();
 })
 

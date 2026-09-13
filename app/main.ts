6b3ac83d31cf02eb9b6e36049426f6b1e55a37f0
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-13 22:30:45 +0545

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,20 +6,36 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
-// TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 
+const KNOWN_CMDS: Record<string, (str: string) => void> = {
+  'echo': (str: string) => console.log(str),
+  'type': (str: string): void => {
+    if (KNOWN_CMDS[str] || str === 'exit') {
+      console.log(`${str} is a shell builtin`);
+    } else {
+      console.log(`${str}: not found`);
+    }
+  },
+  'invalidCmd': (cmd: string) => console.log(`${cmd}: command not found`),
+};
+
 rl.on("line", (command) => {
+  const [cmd, ...input] = command.split(' ');
+  const inputString = input.join(' ');
+
   if (command === "exit") {
     rl.close();
     return;
-  } else if (command.startsWith("echo ")) {
-    console.log(command.slice(5));
-  } else {
-    console.log(`${command}: command not found`);
   }
+
+  const ansFn = KNOWN_CMDS[cmd];
+
+  if (!ansFn) {
+    KNOWN_CMDS['invalidCmd'](command);
+  } else {
+    ansFn(inputString);
+  }
+
   rl.prompt();
-})
-
-
-
+});

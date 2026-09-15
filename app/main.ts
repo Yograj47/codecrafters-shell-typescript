68a57ff26b4db2c0c6f88b6d535efc7b75a5f969
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-15 21:58:30 +0545

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -32,13 +32,26 @@ function parseToken(line: string) {
   const tokens: string[] = [];
   let currentArg = "";
   let inSingleQuote = false;
+  let inDoubleQuote = false;
 
   for (let i = 0; i < line.length; i++) {
     const char = line[i];
 
     if (char === "'") {
-      inSingleQuote = !inSingleQuote
-    } else if (/\s/.test(char) && !inSingleQuote) {
+      if (!inDoubleQuote) {
+        inSingleQuote = !inSingleQuote;
+      } else {
+        currentArg += char;
+      }
+    }
+    else if (char === '"') {
+      if (!inSingleQuote) {
+        inDoubleQuote = !inDoubleQuote;
+      } else {
+        currentArg += char;
+      }
+    }
+    else if (/\s/.test(char) && !inSingleQuote && !inDoubleQuote) {
       if (currentArg.length > 0) {
         tokens.push(currentArg);
         currentArg = "";

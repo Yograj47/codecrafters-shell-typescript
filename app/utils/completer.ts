7eceae35304e7039e0947563a2b50d95a0c054ec
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-20 14:30:39 +0545

    codecrafters submit [skip ci]

--- app/utils/completer.ts
+++ app/utils/completer.ts
@@ -5,5 +5,10 @@ export function completer(line: string): [string[], string] {
 
     const hits = candidates.filter((cmd) => cmd.startsWith(line));
 
+    if (hits.length === 0) {
+        process.stdout.write("\x07");
+        return [[], line];
+    }
+
     return [hits, line];
 }

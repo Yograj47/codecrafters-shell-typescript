9ddca5ddb74b58f6e05b5db29a2366210c994e21
Yograj Rijal <yograjrijal926@gmail.com>
2026-09-21 22:30:41 +0545

    codecrafters submit [skip ci]

--- app/utils/completer.ts
+++ app/utils/completer.ts
@@ -1,3 +1,4 @@
+import * as fs from "node:fs";
 import * as process from "node:process";
 import { BUILTIN_COMMANDS } from "../commands/builtins.js";
 import { getExecutablesFromPath } from "../services/pathResolver.js";
@@ -18,6 +19,36 @@ function findLongestCommonPrefix(strings: string[]): string {
 }
 
 export function completer(line: string): [string[], string] {
+
+    const lastSpaceIndex = line.lastIndexOf(" ");
+
+    if (lastSpaceIndex !== -1) {
+        const commandPrefix = line.slice(0, lastSpaceIndex + 1);
+        const argPrefix = line.slice(lastSpaceIndex + 1);
+        try {
+            const files = fs.readdirSync(process.cwd());
+            const hits = files.filter(f => f.startsWith(argPrefix)).sort();
+
+            if (hits.length === 0) {
+                process.stdout.write("\x07");
+                return [[], line];
+            }
+
+            if (hits.length === 1) {
+                return [[commandPrefix + hits[0] + " "], line];
+            }
+            const lcp = findLongestCommonPrefix(hits);
+            if (lcp.length > argPrefix.length) {
+                return [[commandPrefix + lcp], line];
+            }
+
+            return [[commandPrefix + hits[0] + " "], line];
+        } catch {
+            process.stdout.write("\x07");
+            return [[], line];
+        }
+    }
+
     const allCommands = new Set<string>([
         ...BUILTIN_COMMANDS,
         ...getExecutablesFromPath(),
